d2d4c0cdb572eaf30952d43611f568fa93d25ca8
Matheus Palmeira <matheus_160497@hotmail.com>
2020-09-17 04:35:33 -0300

    server

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import express from 'express'
-
 const app = express()
 
-app.use('/static', express.static(__dirname + '/../bialimakeup-site'))
+app.use('/', express.static('../bialimakeup-site'))
 app.listen(3000, () => {
   console.log(`Example app listening at http://localhost:3000`)
 })
